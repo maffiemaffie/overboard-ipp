01921cf450cb27525856d700a1153a9ce420ec04
clh5602 <clh5602@rit.edu>
2025-03-25 08:15:55 -0400

    coin anim

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -4,10 +4,27 @@ public class Coin : MonoBehaviour
 {
     public int points = 100; // Points awarded when the coin is collected
     private bool isCollected = false;
+    private bool isVisible = false;
+    private Animator animator; // Reference to the Animator component
+
+    void Start()
+    {
+        // Start the coin as invisible and with collision disabled
+        gameObject.SetActive(false);
+        GetComponent<Collider>().enabled = false;
+        animator = GetComponent<Animator>();
+    }
 
     void Update()
     {
-        if (!isCollected)
+        if (!isCollected && !isVisible && transform.position.y > 0)
+        {
+            // Play the sprite animation
+            animator.Play("CoinAppear"); // Assuming "CoinAppear" is the name of the animation
+            isVisible = true;
+        }
+
+        if (isVisible && !isCollected)
         {
             // Make the coin spin around the Y-axis (using deltaTime to make it frame-rate independent)
             transform.Rotate(100f * Time.deltaTime, 0, 0); // 100f is the rotation speed
@@ -31,5 +48,12 @@ public class Coin : MonoBehaviour
         // Set the coin to be collected and disable it (it will disappear)
         isCollected = true;
         gameObject.SetActive(false); // Coin disappears after collection
+    }
+
+    // This method should be called at the end of the "CoinAppear" animation
+    public void OnAnimationEnd()
+    {
+        gameObject.SetActive(true);
+        GetComponent<Collider>().enabled = true;
     }
 }
